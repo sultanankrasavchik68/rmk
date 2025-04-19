d81f04d6c44602e158ccaaef134331636e1297d2
sultanankrasavchik68 <sultananvarov34@gmail.com>
2025-04-19 09:17:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let vopros = prompt("Backgroundni rangini kiriting")
+let voprosn2 = prompt("Textni rangini yozing")
+
+if( vopros == 'blue' ){
+  document.getElementById("nice").style.background = "blue";
+}
+
+if( vopros == 'purple' ){
+    document.getElementById("nice").style.background = "purple";
+  }
+
+  if( vopros == 'black' ){
+    document.getElementById("nice").style.background = "black";
+  }
+
+  if( vopros == 'white' ){
+    document.getElementById("nice").style.background = "white";
+  }
+
+  if( voprosn2.toUpperCase == 'green' ){
+    document.getElementById("nice").style.color = "green";
+  }
+
+  if( voprosn2 == 'blue' ){
+    document.getElementById("nice").style.color = "blue";
+  }
+  
+  if( voprosn2 == 'purple' ){
+      document.getElementById("nice").style.color = "purple";
+    }
+  
+    if( voprosn2.toUpperCase == 'black' ){
+      document.getElementById("nice").style.color = "black";
+    }
+  
+    if( voprosn2 == 'white' ){
+      document.getElementById("nice").style.color = "white";
+    }
+  
+    if( voprosn2 == 'green' ){
+      document.getElementById("nice").style.color = "green";
+    }
